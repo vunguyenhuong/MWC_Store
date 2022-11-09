02b1dde4b7098d651d2d9b537cfa5f6650a4b003
LaiChi15 <chiltkph26384@fpt.edu.vn>
2022-11-09 16:23:34 +0700

    em vua sua file ap

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -7,8 +7,10 @@ import views.SplashScreen;
  * @author VU NGUYEN HUONG
  */
 public class Application {
+
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("huong vnnnnnnn");
         new SplashScreen().setVisible(true);
     }
 }
