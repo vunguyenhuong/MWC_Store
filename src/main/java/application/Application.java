16edfcab12edfe74619a13e53881c659bab6bbf7
VuHuong0510 <lmht1717@gmail.com>
2022-11-09 16:29:54 +0700

    Mình vừa sửa file Application hihi

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -11,6 +11,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("huong vnnnnnnn");
+        System.out.println("kimchicute");
         new SplashScreen().setVisible(true);
     }
 }
