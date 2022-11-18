fedc780fc372f8aff6cccb20f890f1baf1a2d988
phuongdtY <homnayphuonglonglanh@gmail.com>
2022-11-18 07:58:28 +0700

    model khach hang

--- src/main/java/models/KhachHang.java
+++ src/main/java/models/KhachHang.java
@@ -1,9 +1,48 @@
 package models;
 
-/**
- *
- * @author VU NGUYEN HUONG
- */
-public class KhachHang {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "KhachHang")
+
+public class KhachHang implements  Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     
+    @Column(name = "Ma")
+    private String ma;
+    
+    @Column(name = "Ten")
+    private String ten;
+    
+    @Column(name = "Sdt")
+    private String soDt;
+    
+    @Column(name = "DiaChi")
+    private String diaChi;
+    
+    @Column(name = "DiemTichLuy")
+    private int diemTichLuy; 
+    
+    @Override
+    public String toString() {
+        return ten;
+    }
 }
