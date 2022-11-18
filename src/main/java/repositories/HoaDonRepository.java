21e0cadb744393d8ad181a74611cd9ae413b7194
GiangHV231203 <gianghvph23041@fpt.edu.vn>
2022-11-18 09:07:42 +0700

    Repo and Service HoaDon

--- src/main/java/repositories/HoaDonRepository.java
+++ src/main/java/repositories/HoaDonRepository.java
@@ -20,7 +20,60 @@ public class HoaDonRepository {
         List<HoaDon> list = query.getResultList();
         return list;
     }
+    public boolean save(HoaDon hd){
+        try {
+            transaction.begin();
+            session.saveOrUpdate(hd);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e);
+            return false;
+        }
+    }
     
+    public boolean delete(HoaDon hd){
+        try {
+            transaction.begin();
+            session.delete(hd);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e);
+            return false;
+        }
+    }
+    
+    public HoaDon getObj(String ma){
+        HoaDon hd = null;
+        try {
+            Query query = session.createQuery(" SELECT h FROM HoaDon h WHERE h.ma = :ma");
+            query.setParameter("ma", ma);
+            hd = (HoaDon) query.getSingleResult();
+            
+        } catch (Exception e) {
+        }
+        return hd;
+    }
+    
+    public HoaDon getObjById(int id){
+        HoaDon hd = null;
+        try {
+            Query query = session.createQuery(" SELECT d FROM HoaDon d WHERE d.id = :id ");
+            query.setParameter("id", id);
+            hd = (HoaDon) query.getSingleResult();
+        } catch (Exception e) {
+        }
+        return hd;
+    }
+    public List<HoaDon> findByName(String ma){
+        Query query = session.createQuery(" SELECT hd FROM HoaDon hd WHERE hd.ma like :ma ");
+        query.setParameter("ma","%" + ma + "%");
+        List<HoaDon> list1 = query.getResultList();
+        return list1;
+    }
     public static void main(String[] args) {
         HoaDonRepository hdr = new HoaDonRepository();
         for (HoaDon x : hdr.getAll()) {
