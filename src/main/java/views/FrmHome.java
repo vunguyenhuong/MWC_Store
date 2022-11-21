dce7f709cd951d80338947d5e4c85e9d908fa0f5
VuHuong0510 <lmht1717@gmail.com>
2022-11-21 09:00:47 +0700

    up

--- src/main/java/views/FrmHome.java
+++ src/main/java/views/FrmHome.java
@@ -65,12 +65,12 @@ import utilities.Helper;
  * @author KenTizz
  */
 public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFactory {
-
+    
     private static final long serialVersionUID = 6441489157408381878L;
     private Executor executor = Executors.newSingleThreadExecutor(this);
     private Webcam webcam = null;
     private WebcamPanel panel = null;
-
+    
     private Helper helper = new Helper();
     private MigLayout layout;
     private Menu menu;
@@ -87,7 +87,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
     private IKhachHangService iKhachHangService = new KhachHangService();
     private IKhuyenMaiService iKhuyenMaiService = new KhuyenMaiService();
     private CardLayout cardLayout;
-
+    
     public FrmHome(NguoiDung nd) {
         initComponents();
         Image icon = Toolkit.getDefaultToolkit().getImage("images/suportUI/logo.png");
@@ -104,7 +104,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
         loadSP(iChiTietDepService.findByTT(0, ""));
         loadHD(iHoaDonService.getAll());
     }
-
+    
     public FrmHome() {
         initComponents();
         setExtendedState(MAXIMIZED_BOTH);
@@ -117,7 +117,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
         Table.apply(jScrollPane3, Table.TableType.MULTI_LINE);
         loadSP(iChiTietDepService.findByTT(0, ""));
     }
-
+    
     private void init() {
         layout = new MigLayout("fill", "0[]0[100%, fill]0", "0[fill, top]0");
         bg.setLayout(layout);
@@ -182,9 +182,9 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
                 }
                 if (menuIndex == 3) {
                     if (subMenuIndex == 0) {
-
+                        
                     } else if (subMenuIndex == 1) {
-
+                        
                     }
                 }
                 if (menuIndex == 4) {
@@ -192,16 +192,16 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
                     loadSP(iChiTietDepService.getAll());
                     initWebcam(pn_webcam);
                     if (subMenuIndex == 0) {
-
+                        
                     } else if (subMenuIndex == 1) {
-
+                        
                     }
                 }
                 if (menuIndex == 5) {
                     if (subMenuIndex == 0) {
-
+                        
                     } else if (subMenuIndex == 1) {
-
+                        
                     }
                 }
                 if (menuIndex == 6) {
@@ -249,13 +249,13 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
                 layout.setComponentConstraints(menu, "w " + width + "!, spany2");
                 menu.revalidate();
             }
-
+            
             @Override
             public void end() {
                 menu.setShowMenu(!menu.isShowMenu());
                 menu.setEnableMenu(true);
             }
-
+            
         };
         animator = new Animator(500, target);
         animator.setResolution(0);
@@ -277,7 +277,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
         //  Start with this form
         main.showForm(new FrmTrangChu());
     }
-
+    
     private void loadSP(List<ChiTietDep> list) {
         int stt = 1;
         defaultTableModel = (DefaultTableModel) tb_sanpham.getModel();
@@ -288,7 +288,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
             });
         }
     }
-
+    
     private void loadHD(List<HoaDon> list) {
         defaultTableModel = (DefaultTableModel) tb_hoadon.getModel();
         defaultTableModel.setRowCount(0);
@@ -302,7 +302,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
             });
         }
     }
-
+    
     private void loadGioHang(String maHD) {
         int stt = 1;
         defaultTableModel = (DefaultTableModel) tb_giohang.getModel();
@@ -313,7 +313,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
             });
         }
     }
-
+    
     private void tongTien() {
         int row = tb_giohang.getRowCount();
         if (row > 0) {
@@ -326,7 +326,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
                 txt_giamgia.setText("0");
             }
             giamGia = tongTien - Double.parseDouble(txt_giamgia.getText());
-
+            
             txt_tongtien.setText(String.valueOf(tongTien));
             txt_phaitra.setText(String.valueOf(giamGia));
         } else {
@@ -343,7 +343,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
 //        } catch (Exception e) {
 //        }
     }
-
+    
     private void addSpToGH() {
         int row = tb_sanpham.getSelectedRow();
         int rowHD = tb_hoadon.getSelectedRow();
@@ -406,7 +406,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
         }
         tongTien();
     }
-
+    
     private void clearForm() {
         tb_giohang.clearSelection();
         tb_hoadon.clearSelection();
@@ -427,7 +427,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
         txt_tienkhachdua.setText("");
         txt_tienthua.setText("");
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -997,7 +997,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
         } catch (Exception e) {
         }
     }//GEN-LAST:event_btn_returnActionPerformed
-
+    
     private int checkSearchSP = 0;
     private void txt_sp_timkiemCaretUpdate(javax.swing.event.CaretEvent evt) {//GEN-FIRST:event_txt_sp_timkiemCaretUpdate
         if (iChiTietDepService.findByTT(0, txt_sp_timkiem.getText()).size() == iChiTietDepService.getAll().size()) {
@@ -1148,7 +1148,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
                 }
             } catch (Exception e) {
             }
-
+            
             if (tienKhachDua >= phaiTra) {
                 if (helper.confirm(this, "Trả lại khách " + (tienKhachDua - phaiTra) + ". Xác nhận thanh toán " + txt_phaitra.getText() + "?")) {
                     hd.setNguoiDungTT(nguoiDung);
@@ -1228,7 +1228,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
                 panel.showNotification();
                 return;
             }
-
+            
             if (soLuong == 0) {
                 if (helper.confirm(this, "Xác nhận xóa " + hdct.getCtdep().getDep().getTen() + " khỏi giỏ hàng ?")) {
                     ctd.setSoLuong(ctd.getSoLuong() + hdct.getSoLuong());
@@ -1381,7 +1381,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
         loadGioHang(hd.getMa());
         tongTien();
     }//GEN-LAST:event_tb_hoadonMousePressed
-
+    
     public static void main(String args[]) {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -1393,7 +1393,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -1454,17 +1454,17 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
         Dimension size = WebcamResolution.QVGA.getSize();
         webcam = Webcam.getWebcams().get(0); //0 is default webcam
         webcam.setViewSize(size);
-
+        
         panel = new WebcamPanel(webcam);
         panel.setPreferredSize(size);
         panel.setFPSDisplayed(true);
         panel.setMirrored(true);
-
+        
         panelShow.add(panel, new AbsoluteConstraints(0, 0, panelShow.getWidth(), panelShow.getHeight()));
-
+        
         executor.execute(this);
     }
-
+    
     @Override
     public void run() {
         while (true) {
@@ -1473,26 +1473,26 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
+            
             Result result = null;
             BufferedImage image = null;
-
+            
             if (webcam.isOpen()) {
-
+                
                 if ((image = webcam.getImage()) == null) {
                     continue;
                 }
-
+                
                 LuminanceSource source = new BufferedImageLuminanceSource(image);
                 BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
-
+                
                 try {
                     result = new MultiFormatReader().decode(bitmap);
                 } catch (NotFoundException e) {
                     // fall thru, it means there is no QR code in image
                 }
             }
-
+            
             if (result != null) {
                 try {
                     String maSP = result.getText().replace("MWCSTORES", "");
@@ -1507,7 +1507,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
+                
                 try {
                     HoaDon hd = iHoaDonService.getObj(txt_mahd.getText());
                     String maKhuyenMai = result.getText().replace("MWCSTORES", "");
@@ -1557,7 +1557,7 @@ public class FrmHome extends javax.swing.JFrame implements Runnable, ThreadFacto
             }
         }
     }
-
+    
     @Override
     public Thread newThread(Runnable r) {
         Thread t = new Thread(r, "example-runner");
