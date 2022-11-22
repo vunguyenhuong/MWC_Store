f24cfdd7267fdb6ca85b82867c2dfdc969e06ba7
VuHuong0510 <lmht1717@gmail.com>
2022-11-22 13:58:33 +0700

    check trùng + số lượng khi import excel

--- src/main/java/views/FrmChiTietDep.java
+++ src/main/java/views/FrmChiTietDep.java
@@ -572,11 +572,26 @@ public class FrmChiTietDep extends javax.swing.JPanel {
                 List<ChiTietDep> list = ImportSP.readExcel(fileOpen.getAbsolutePath());
                 if (helper.confirm(this, "Xác nhận thêm " + list.size() + " sản phẩm ?")) {
                     for (ChiTietDep x : list) {
-                        iChiTietDepService.save(x);
+                        ChiTietDep ctd = iChiTietDepService.getObjByProperties(x.getDep().getId(), x.getLoaiDep().getId(), x.getMauSac().getId(), x.getChatLieu().getId(), x.getNhaSX().getId(), x.getSize().getId());
+                        if (ctd != null) {
+                            ctd.setSoLuong(ctd.getSoLuong() + x.getSoLuong());
+                            iChiTietDepService.save(ctd);
+                            continue;
+                        } else {
+                            iChiTietDepService.save(x);
+                        }
+
                     }
                     loadData(iChiTietDepService.getAll());
                     helper.alert(this, "Thêm thành công!");
                 }
+//                if (helper.confirm(this, "Xác nhận thêm " + list.size() + " sản phẩm ?")) {
+//                    for (ChiTietDep x : list) {
+//                        iChiTietDepService.save(x);
+//                    }
+//                    loadData(iChiTietDepService.getAll());
+//                    helper.alert(this, "Thêm thành công!");
+//                }
             } catch (Exception e) {
                 e.printStackTrace();
                 helper.alert(this, "Add File thất bại!");
