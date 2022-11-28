994b485dbea6fc3d82ba128e836d3492cc4701d3
VuHuong0510 <lmht1717@gmail.com>
2022-11-28 20:15:12 +0700

    xóa code không dùng tới trong repo Chi Tiết dép

--- src/main/java/repositories/ChiTietDepRepository.java
+++ src/main/java/repositories/ChiTietDepRepository.java
@@ -50,21 +50,6 @@ public class ChiTietDepRepository {
         }
     }
 
-//    public List<ChiTietDep> findByName(String ten) {
-//        Query query = session.createQuery("SELECT c FROM ChiTietDep c WHERE c.dep.ten LIKE :ten");
-//        query.setParameter("ten", "%" + ten + "%");
-//        List<ChiTietDep> list = query.getResultList();
-//        return list;
-//    }
-//    public List<ChiTietDep> filter(String tenDep, String tenMauSac) {
-//        Query query = session.createQuery("SELECT c FROM ChiTietDep c "
-//                + " WHERE (c.dep.ten = :tenDep or :tenDep is null or :tenDep = '')"
-//                + " AND (c.mauSac.ten = :tenMauSac or :tenMauSac is null or :tenMauSac = '')");
-//        query.setParameter("tenDep", tenDep);
-//        query.setParameter("tenMauSac", tenMauSac);
-//        List<ChiTietDep> list = query.getResultList();
-//        return list;
-//    }
     public List<ChiTietDep> findByTT(int trangThai, String ten) {
         Query query = session.createQuery("SELECT c FROM ChiTietDep c WHERE c.trangThai = :trangThai AND c.dep.ten LIKE :ten");
         query.setParameter("trangThai", trangThai);
@@ -91,15 +76,6 @@ public class ChiTietDepRepository {
         return ctd;
     }
 
-//    public List<ChiTietDep> pagination(int pageNumber, int pageSize) {
-//        Query query = session.createQuery("SELECT c FROM ChiTietDep c");
-//        int pageIndex = pageNumber - 1 < 0 ? 0 : pageNumber - 1;
-//        int fromRecordIndex = pageIndex * pageSize;
-//        query.setFirstResult(fromRecordIndex);
-//        query.setMaxResults(pageSize);
-//        List<ChiTietDep> list = query.getResultList();
-//        return list;
-//    }
     public ChiTietDep checkDuplicate(int idDep, int idLoaiDep, int idMauSac, int idChatLieu, int idNSX, int idSize) {
         ChiTietDep ctd = null;
         try {
