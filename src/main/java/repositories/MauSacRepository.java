6d99ecf86e903b0a142042a217631854cfbb7e41
canhdv281 <canhdat28@gmail.com>
2022-12-01 22:20:47 +0700

    sort date

--- src/main/java/repositories/MauSacRepository.java
+++ src/main/java/repositories/MauSacRepository.java
@@ -67,7 +67,7 @@ public class MauSacRepository {
     }
 
     public List<MauSac> pagination(int pageNumber, int pageSize, String ten) {
-        Query query = session.createQuery("SELECT n FROM MauSac n WHERE n.ten like :ten");
+        Query query = session.createQuery("SELECT n FROM MauSac n WHERE n.ten like :ten ORDER BY n.ngaySuaCuoi DESC");
         int pageIndex = pageNumber - 1 < 0 ? 0 : pageNumber - 1;
         int fromRecordIndex = pageIndex * pageSize;
         query.setFirstResult(fromRecordIndex);
